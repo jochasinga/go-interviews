604d10be402f0ae4e537b6287e6614493da8b273
Joe Chasinga <jo.chasinga@gmail.com>
2016-05-17 02:56:38 -0400

    Add fizzbuzz

---
+++ questions/fizzbuzz.go
@@ -0,0 +1,22 @@
+// Print 1 to 100, but for every multiple of 3 print "fizz" and multiple
+// of 5 print "buzz" and for multiple of both 3 and 5 print "fizzbuzz"
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	for i := 1; i < 100; i++ {
+		fmt.Println(i)
+		if i%3 == 0 {
+			fmt.Println("fizz")
+		}
+		if i%5 == 0 {
+			fmt.Println("buzz")
+		}
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("fizzbuzz")
+		}
+	}
+}
