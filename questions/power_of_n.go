a58ad3bc529c85389235ddd252fcd32393dd9bc7
Joe Chasinga <jo.chasinga@gmail.com>
2016-05-29 19:17:27 -0400

    Fix

--- questions/power_of_n.go
+++ questions/power_of_n.go
@@ -4,25 +4,35 @@ import "fmt"
 
 // Finding out if n is a power of some number can be done
 // by dividing n by the number recursively until n = that number.
-func isPowerOfFour(n uint32) bool {
+func isPowerOfFour(n, power uint32) bool {
 
-	n /= 4
-
-	switch {
-	case n == 4:
+	if n == 1 || n == power {
 		return true
-	case n > 4:
-		return isPowerOfFour(num)
-	default:
-		break
+	}
+
+	if n > 1 && n % power == 0 {
+		
+		n /= power
+
+		switch {
+		case n == power:
+			return true
+		case n > power:
+			return isPowerOfFour(n, power)
+		default:
+			break
+		}
 	}
 	
 	return false
 }
 		
 func main() {
-	fmt.Println(isPowerOfFour(8))
-	fmt.Println(isPowerOfFour(16))
-	fmt.Println(isPowerOfFour(10))
-	fmt.Println(isPowerOfFour(64))
+	fmt.Println(isPowerOfFour(0, 4))
+	fmt.Println(isPowerOfFour(1, 4))
+	fmt.Println(isPowerOfFour(8, 4))
+	fmt.Println(isPowerOfFour(16, 4))
+	fmt.Println(isPowerOfFour(10, 2))
+	fmt.Println(isPowerOfFour(64, 8))
+	fmt.Println(isPowerOfFour(17, 3))
 }
