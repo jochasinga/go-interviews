01ba5c2b1fc6daf4d1e2022a3f7d833d2cdd64a6
Joe Chasinga <jo.chasinga@gmail.com>
2016-05-29 16:13:35 -0400

    Edit to include a param

--- data_structure/closure/fibonacci_closure.go
+++ data_structure/closure/fibonacci_closure.go
@@ -2,28 +2,33 @@ package main
 
 import "fmt"
 
-// Very naive answer.
 // fibonacci is a function that returns
-// a function that returns an int.
-func fibonacci() func() int {
-	n := 0
-	a := 0
-	b := 1
-	c := a + b
-	return func() int {
+// a function that returns an int, so on and so forth.
+// fib: prev, recent, prev + recent, ...
+// fib(0) = 0
+// fib(1) = 1
+func fibonacci() func(int) int {
+	// Start from 0
+	prev := 0
+	// Then recent 1
+	recent := 1
+	// Current is 1
+	current := prev + recent
+	return func(n int) int {
 		var ret int
-		switch {
-		case n == 0:
+		switch n {
+		// Handle each case
+		case 0:
 			n++
 			ret = 0
-		case n == 1:
+		case 1:
 			n++
 			ret = 1
 		default:
-			ret = c
-			a = b
-			b = c
-			c = a + b
+			ret = current
+			prev = recent
+			recent = current
+			current = prev + recent
 		}
 		return ret
 	}
@@ -31,7 +36,9 @@ func fibonacci() func() int {
 
 func main() {
 	f := fibonacci()
+
 	for i := 0; i < 10; i++ {
-		fmt.Println(f())
+		fmt.Println(f(i))
 	}
+
 }
